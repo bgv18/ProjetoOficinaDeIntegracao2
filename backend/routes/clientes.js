f533428e08e011a6a302955c40870f163aead16f
Igor <IgorBregagnoli1@gmail.com>
2022-11-07 20:13:35 -0300

    Atualizando Backend de Clientes

--- backend/routes/clientes.js
+++ backend/routes/clientes.js
@@ -1,69 +1,49 @@
-const { request, response } = require("express");
 const express = require("express");
 const router = express.Router();
 const {Clientes} = require("../models");
 
-router.get("/Clientes", async (req, res) => {
+router.get("/", async (req, res) => {
     const listaClientes = await Clientes.findAll();
     res.json(listaClientes);
 });
 
-const getClienteById = (request, response) => {
-    const id = parseInt(request.params.id);
-    Clientes.query('Select * From Clientes where id = $1', [id], (error, results)=>{
-        if(error){
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
-router.get("/clientes/:id", getClienteById);
+router.get("/byId/:id", async (req, res) => {
+    const id = req.params.id;
+    const cliente = await Clientes.findByPk(id);
+    res.json(cliente);
+});
 
-const getClienteBycpfOuCnpj = (request, response) => {
-    const cpfOuCnpj = parseInt(request.params.cpfOuCnpj);
-    Clientes.query('Select * From Clientes where id = $1', [cpfOuCnpj], (error, results)=>{
-        if(error){
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
-router.get("/clientes/:cpfOuCnpj", getClienteBycpfOuCnpj);
+router.get("/byCpf/:cpf", async (req, res) => {
+    const cpfOuCnpj = req.params.cpf;
+    const cliente = await Clientes.findOne({where: {cpfOuCnpj: cpfOuCnpj}});
+    res.json(cliente);
+});
 
-const getClienteByNome = (request, response) => {
-    const nome = parseInt(request.params.nome);
-    Clientes.query('Select * From Clientes where nome like %$1%', [nome], (error, results)=>{
-        if(error){
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
-router.get("/clientes/:nome", getClienteByNome);
+router.get("/byNome/:nome", async (req, res) => {
+    const nome = req.params.nome;
+    const cliente = await Clientes.findOne({where: {nome: nome}});
+    res.json(cliente);
+});
 
-const getClienteByCidade = (request, response) => {
-    const cidade = parseInt(request.params.cidade);
-    Clientes.query('Select * From Clientes where cidade like %$1%', [cidade], (error, results)=>{
-        if(error){
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
-router.get("/clientes/:cidade", getClienteByCidade);
+router.get("/byCidade/:cidade", async (req, res) => {
+    const cidade = req.params.cidade;
+    const cliente = await Clientes.findAll({where: {cidade: cidade}});
+    res.json(cliente);
+});
 
-const getClienteByEstado = (request, response) => {
-    const estado = parseInt(request.params.estado);
-    Clientes.query('Select * From Clientes where estado like %$1%', [estado], (error, results)=>{
-        if(error){
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
-router.get("/clientes/:estado", getClienteByEstado);
+router.get("/byEstado/:estado", async (req, res) => {
+    const estado = req.params.estado;
+    const cliente = await Clientes.findAll({where: {estado: estado}});
+    res.json(cliente);
+});
 
-router.post("/Clientes", async (req, res) => {
+router.get("/byPais/:pais", async (req, res) => {
+    const pais = req.params.pais;
+    const cliente = await Clientes.findAll({where: {pais: pais}});
+    res.json(cliente);
+});
+
+router.post("/", async (req, res) => {
     const cliente = req.body;
     await Clientes.create(cliente);
     res.json(cliente);
